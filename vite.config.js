10f66d0ea1de67ae4aeeea4fdd04f1ea102a3086
Ivan <0675361905vi@gmail.com>
2025-02-05 17:14:32 +0200

    fix icoons

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base:'/web-studio/',
-  publicDir:'./public/assets',
+  publicDir:'./assets',
   server: {
     open: true,
   },
